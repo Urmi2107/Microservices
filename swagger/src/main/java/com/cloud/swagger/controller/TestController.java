ef62668a5199485e647cda74707828ecec8b28b0
Urmi2107 <42772510+Urmi2107@users.noreply.github.com>
2018-10-13 12:10:27 +0530

    Swagger

---
+++ swagger/src/main/java/com/cloud/swagger/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.cloud.swagger.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping(value="/hello")
+	public String hello(){
+		return "Hello World";		
+	}
+}
